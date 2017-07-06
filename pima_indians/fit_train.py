bb4734dc2d03a8ab8f990e6d165e4072d7648789
k3170makan <k3170makan@gmail.com>
2017-07-06 03:53:56 +0200

    pima indians done

--- pima_indians/fit_train.py
+++ pima_indians/fit_train.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import numpy
 from data import DATA_LIB
 from keras.models import Sequential
@@ -5,7 +6,7 @@ from keras.layers import Dense
 
 def build():
 	model = Sequential()
-	model.add(Dense(12,input_dim='8',activation='relu')) #input layer
+	model.add(Dense(12,input_dim=8,activation='relu')) #input layer
 	model.add(Dense(8,activation='relu')) #hidden layer
 	model.add(Dense(1,activation='sigmoid')) #output layer
 	#could handle errors here in isolation ;)	
@@ -15,12 +16,23 @@ def fit(dataset, model):
 	X = dataset[:,0:8] #get first 8 fields
 	Y = dataset[:,8] #use 9th field as output 1 0r 0
 
-	mode.fit(X,Y,epochs=150,batch_size=10)	
+	model.fit(X,Y,epochs=150,batch_size=10)	
 	return #should there be a return value here? 
-def evaluate(model):
-	#get the metrics on the accuracy
-	return
+def evaluate(model,dataset):
+	X = dataset[:,0:8]	
+	Y = dataset[:,8]	
+	return model.evaluate(X,Y)
+def predict(model,dataset):
+	X = dataset[:,0:8]
+	Y = dataset[:,8]
+	predictions = model.predict(X)
+	rounded = [round(x[0]) for x in predictions]
+	return rounded
 if __name__=="__main__":
 	dataset = numpy.loadtxt(DATA_LIB,delimiter=",")
-	
+	model = build()
+	fit(dataset,model)
+	scores = evaluate(model,dataset)
+	print "[*] %s %.2f%%" % (model.metrics_names[1],scores[1]*100)	
+	print "[*] output: ",predict(model,dataset)
 
